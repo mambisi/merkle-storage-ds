cd8426fbd0f45923cc44f3c0ada682ef3cd7f114
Mambisi Zempare <lilbizi96@gmail.com>
2020-12-28 20:37:19 +0000

    bug fix

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,10 +1,11 @@
+#![feature(async_closure)]
 extern crate merkle;
 
 mod channel;
 mod util;
 
 use merkle::prelude::*;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, RwLock, Mutex};
 use rayon::prelude::*;
 use std::time::Instant;
 use rand::prelude::*;
@@ -37,6 +38,7 @@ use termion::event::{Key};
 use tui::text::Span;
 use util::Event;
 use tui::buffer::Cell;
+use tokio::sync::RwLockReadGuard;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -68,21 +70,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .get_matches();
 
-    let node = matches.value_of("node").unwrap();
+    let node = matches.value_of("node").unwrap().to_string();
     let blocks_limit = matches.value_of("limit").unwrap().parse::<u64>().unwrap_or(25000);
     let cycle = matches.value_of("cycle").unwrap().parse::<u64>().unwrap_or(4096);
     let process_id = std::process::id();
 
     println!("node {}, limit {}, process id: {}", node, blocks_limit, process_id);
 
-    let mut ui = BenchUI::default();
-    run_benchmark(process_id, &mut ui, node, blocks_limit, cycle);
-    ui.run()?;
+    let mut ui = Arc::new(RwLock::new(BenchUI::default()));
+    let ui2 = ui.clone();
+    tokio::spawn( async move {
+        let node = node;
+        run_benchmark(process_id, ui2, &node, blocks_limit, cycle).await;
+    });
+    run(ui)?;
     Ok(())
 }
 
 
-async fn run_benchmark(process_id: u32, ui: &mut BenchUI, node: &str, blocks_limit: u64, cycle: u64) -> Result<(), Box<dyn std::error::Error>> {
+async fn run_benchmark(process_id: u32, ui: Arc<RwLock<BenchUI>>, node: &str, blocks_limit: u64, cycle: u64) -> Result<(), Box<dyn std::error::Error>> {
     let blocks_url = format!("{}/dev/chains/main/blocks?limit={}&from_block_id={}", node, blocks_limit + 10, blocks_limit);
     let db = Arc::new(RwLock::new(DB::new()));
     let mut storage = MerkleStorage::new(db.clone());
@@ -95,7 +101,11 @@ async fn run_benchmark(process_id: u32, ui: &mut BenchUI, node: &str, blocks_lim
 
     blocks.reverse();
 
-    ui.total_blocs = blocks.len() as f64;
+    {
+        let mut ui = ui.write().unwrap();
+        ui.total_blocs = blocks.len() as f64;
+    }
+
 
     for block in blocks {
         let block = block.as_object().unwrap();
@@ -152,10 +162,13 @@ async fn run_benchmark(process_id: u32, ui: &mut BenchUI, node: &str, blocks_lim
             };
         }
 
-        ui.synced_blocks = block_level as f64;
+
+
 
         match storage.get_merkle_stats() {
             Ok(stats) => {
+                let mut ui = ui.write().unwrap();
+                ui.synced_blocks = block_level as f64;
                 ui.stats = Some(stats)
             }
             Err(_) => {}
@@ -176,9 +189,13 @@ async fn run_benchmark(process_id: u32, ui: &mut BenchUI, node: &str, blocks_lim
                     .output().await;
                 match output {
                     Ok(output) => {
+                        let mut ui = ui.write().unwrap();
+                        ui.synced_blocks = block_level as f64;
                         ui.logs.push(format!("{}", String::from_utf8_lossy(&output.stdout)))
                     }
                     Err(_) => {
+                        let mut ui = ui.write().unwrap();
+                        ui.synced_blocks = block_level as f64;
                         ui.logs.push(String::from("Error executing PS"));
                     }
                 }
@@ -188,6 +205,8 @@ async fn run_benchmark(process_id: u32, ui: &mut BenchUI, node: &str, blocks_lim
             println!("DB stats (Before GC)  at cycle: {}", current_cycle);
             match storage.get_merkle_stats() {
                 Ok(stats) => {
+                    let mut ui = ui.write().unwrap();
+                    ui.synced_blocks = block_level as f64;
                     ui.logs.push(format!("{:#?}", stats));
                 }
                 Err(_) => {}
@@ -196,6 +215,8 @@ async fn run_benchmark(process_id: u32, ui: &mut BenchUI, node: &str, blocks_lim
             println!("DB stats (After GC)  at cycle: {}", current_cycle);
             match storage.get_merkle_stats() {
                 Ok(stats) => {
+                    let mut ui = ui.write().unwrap();
+                    ui.synced_blocks = block_level as f64;
                     ui.logs.push(format!("{:#?}", stats));
                 }
                 Err(_) => {}
@@ -223,106 +244,105 @@ impl Default for BenchUI {
     }
 }
 
-impl BenchUI {
-    fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        // Terminal initialization
-        let stdout = io::stdout().into_raw_mode()?;
-        let stdout = MouseTerminal::from(stdout);
-        let stdout = AlternateScreen::from(stdout);
-        let backend = TermionBackend::new(stdout);
-        let mut terminal = Terminal::new(backend)?;
+fn run(app : Arc<RwLock<BenchUI>>) -> Result<(), Box<dyn std::error::Error>> {
+    let app = app.read().unwrap();
+    // Terminal initialization
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-        let events = util::Events::new();
-        loop {
-            terminal.draw(|f| {
-                let text: String = String::from("Test Strings");
-                let chunks = Layout::default()
-                    .direction(Direction::Horizontal)
-                    .margin(1)
-                    .constraints(
-                        [
-                            Constraint::Percentage(70),
-                            Constraint::Percentage(30),
-                        ]
-                            .as_ref(),
-                    )
-                    .split(f.size());
+    let events = util::Events::new();
+    loop {
+        terminal.draw(|f| {
+            let text: String = String::from("Test Strings");
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(70),
+                        Constraint::Percentage(30),
+                    ]
+                        .as_ref(),
+                )
+                .split(f.size());
 
-                let left_chunck = Layout::default()
-                    .direction(Direction::Vertical)
-                    .margin(2)
-                    .constraints(
-                        [
-                            Constraint::Length(3),
-                            Constraint::Max(100),
-                        ]
-                            .as_ref(),
-                    )
-                    .split(chunks[0]);
-                let right_chunck = Layout::default()
-                    .direction(Direction::Vertical)
-                    .margin(2)
-                    .constraints(
-                        [
-                            Constraint::Length(3),
-                            Constraint::Length(10),
-                        ]
-                            .as_ref(),
-                    )
-                    .split(chunks[1]);
+            let left_chunck = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Max(100),
+                    ]
+                        .as_ref(),
+                )
+                .split(chunks[0]);
+            let right_chunck = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Length(10),
+                    ]
+                        .as_ref(),
+                )
+                .split(chunks[1]);
 
 
-                let gauge = Gauge::default()
-                    .block(Block::default().title("Sync Progress").borders(Borders::ALL))
-                    .gauge_style(Style::default().fg(Color::Yellow)).ratio(self.synced_blocks / if self.total_blocs == 0.0 { 1_f64 } else { self.total_blocs });
-                f.render_widget(gauge, left_chunck[0]);
+            let gauge = Gauge::default()
+                .block(Block::default().title("Sync Progress").borders(Borders::ALL))
+                .gauge_style(Style::default().fg(Color::Yellow)).ratio(app.synced_blocks / if app.total_blocs == 0.0 { 1_f64 } else { app.total_blocs });
+            f.render_widget(gauge, left_chunck[0]);
 
-                let logs: String = self.logs.join("\n");
+            let logs: String = app.logs.join("\n");
 
-                let paragraph = Paragraph::new(logs)
-                    .style(Style::default().bg(Color::Black).fg(Color::White))
-                    .block(Block::default().title("Logs").borders(Borders::ALL))
-                    .alignment(Alignment::Left)
-                    .wrap(Wrap { trim: true });
-                f.render_widget(paragraph, left_chunck[1]);
+            let paragraph = Paragraph::new(logs)
+                .style(Style::default().bg(Color::Black).fg(Color::White))
+                .block(Block::default().title("Logs").borders(Borders::ALL))
+                .alignment(Alignment::Left)
+                .wrap(Wrap { trim: true });
+            f.render_widget(paragraph, left_chunck[1]);
 
-                let sync_process = if self.total_blocs + self.synced_blocks > 0.0 {
-                    format!("{}/{}", self.synced_blocks, self.total_blocs)
-                } else {
-                    String::from("--/--")
-                };
+            let sync_process = if app.total_blocs + app.synced_blocks > 0.0 {
+                format!("{}/{}", app.synced_blocks, app.total_blocs)
+            } else {
+                String::from("--/--")
+            };
 
-                let paragraph = Paragraph::new(sync_process)
-                    .style(Style::default().bg(Color::Black).fg(Color::White))
-                    .block(Block::default().title("Sync Progress").borders(Borders::ALL))
-                    .alignment(Alignment::Left)
-                    .wrap(Wrap { trim: true });
-                f.render_widget(paragraph, right_chunck[0]);
+            let paragraph = Paragraph::new(sync_process)
+                .style(Style::default().bg(Color::Black).fg(Color::White))
+                .block(Block::default().title("Sync Progress").borders(Borders::ALL))
+                .alignment(Alignment::Left)
+                .wrap(Wrap { trim: true });
+            f.render_widget(paragraph, right_chunck[0]);
 
-                let db_stats = if let Some(stats) = &self.stats {
-                    format!("{:#?}", stats)
-                } else {
-                    String::from("...")
-                };
+            let db_stats = if let Some(stats) = &app.stats {
+                format!("{:#?}", stats)
+            } else {
+                String::from("...")
+            };
 
-                let paragraph = Paragraph::new(db_stats)
-                    .style(Style::default().bg(Color::Black).fg(Color::White))
-                    .block(Block::default().title("Database Stats").borders(Borders::ALL))
-                    .alignment(Alignment::Left)
-                    .wrap(Wrap { trim: true });
-                f.render_widget(paragraph, right_chunck[1]);
-            });
-            match events.next()? {
-                Event::Input(input) => {
-                    if input == Key::Ctrl('c') {
-                        break;
-                    }
+            let paragraph = Paragraph::new(db_stats)
+                .style(Style::default().bg(Color::Black).fg(Color::White))
+                .block(Block::default().title("Database Stats").borders(Borders::ALL))
+                .alignment(Alignment::Left)
+                .wrap(Wrap { trim: true });
+            f.render_widget(paragraph, right_chunck[1]);
+        });
+        match events.next()? {
+            Event::Input(input) => {
+                if input == Key::Ctrl('c') {
+                    break;
                 }
-                Event::Tick => {}
             }
+            Event::Tick => {}
         }
-
-        Ok(())
     }
+
+    Ok(())
 }
 
